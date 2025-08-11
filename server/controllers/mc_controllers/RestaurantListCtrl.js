4d98839c17011c9649079b51d19d53b0a3853daf
charfed <charfedchaibi@gmail.com>
2025-08-11 14:16:59 +0100

    wip 12

--- server/controllers/mc_controllers/RestaurantListCtrl.js
+++ server/controllers/mc_controllers/RestaurantListCtrl.js
@@ -25,45 +25,49 @@ const getUserById = async (req,res)=> {
   }
 }
 
-const getUserPictureById = async(req,res)=> { 
+// const getUserPictureById = async(req,res)=> { 
 
-  try {
-    const userId = req.user.userId;
-     const userPic = await prisma.user.findUnique({
-      where: { id: userId },
-      include: {
-        media: {
-          where: { type: 'image' },
-          take: 1
-        }
-      }
-    });
-console.log('userPic:', userPic);
-  if (!userPic || !userPic.media || userPic.media.length === 0) {
-  return res.status(404).json({ error: "User picture not found" });
-}
+//   try {
+//     const userId = req.user.userId;
+//      const userPic = await prisma.user.findUnique({
+//       where: { id: userId },
+//       include: {
+//         media: {
+//           where: { type: 'image' },
+//           take: 1
+//         }
+//       }
+//     });
+// console.log('userPic:', userPic);
+//   if (!userPic || !userPic.media || userPic.media.length === 0) {
+//   return res.status(404).json({ error: "User picture not found" });
+// }
 
-    res.json(userPic.media[0]?.url || null);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
+//     res.json(userPic.media[0]?.url || null);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
 
 
 const updateUserProfile = async (req, res) => {
   const userId = req.user.userId;
+  const profileImageFile = req.file;
+
   const { fullName, email, phoneNumber, address } = req.body;
 
   try {
+    const updateProfile = { fullName,email,phoneNumber,address }
+      if (profileImageFile) {
+        const imageUrl = `/uploads/profile_pic/${profileImageFile.filename}`
+
+      updateProfile.profileImage = imageUrl;
+    }
+
     const updatedUser = await prisma.user.update({
       where: { id: userId },
-      data: {
-        fullName,
-        email,
-        phoneNumber,
-        address
-      },
+      data: updateProfile,
     });
 
     res.json(updatedUser);
@@ -118,4 +122,4 @@ const updateUserLocation = async (req, res) => {
 
 
 
-module.exports = { getUserById, getAllRestaurants ,updateUserProfile ,getUserPictureById, updateUserLocation }
+module.exports = { getUserById, getAllRestaurants ,updateUserProfile ,/*getUserPictureById,*/ updateUserLocation }
